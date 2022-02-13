558120b2632deed167883db0bf3169dcf5bf4f35
ananias18 <euananias@gmail.com>
2022-02-13 19:37:40 -0300

    Classe de variaveis errada

---
+++ Aula-variavel-java/src/one/digitalinnovation/basecamp/auladevariavel.java
@@ -0,0 +1,50 @@
+package one.digitalinnovation.basecamp;
+
+/*classe de exemplos para o exercio de aula 1 de variáveis,
+tipos dado e operadores aritimeticos.*/
+
+public class auladevariavel {
+    public static void main(String[] args) {
+        int i;
+        int $aq;
+        int I;
+        //int 1a;
+        int _1a;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn2$4678_md;
+        //int asrn246%78_md;
+
+         asrn24678md = 100;
+         asrn24678md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int Numero_TENTATIVAS = 5;
+        //final int numeroTENTATIVAS = 5;
+        int QUANTIDADE_OPCOES = 25;
+        int qtdProd;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        //System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(Numero_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+    }
+}
